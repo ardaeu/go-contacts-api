7cce3020ef45c81ab91bd42507b2f5359fda3a9d
Arda Eymen Ulus <ulusarda9@gmail.com>
2025-08-04 22:36:26 +0300

    Added main application setup with HTTP routes and DB connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,34 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/ardaeu/contact-api/config"
-	"github.com/ardaeu/contact-api/handler"
+	"github.com/ardaeu/go-contacts-api/internal/handler"
+	"github.com/ardaeu/go-contacts-api/internal/storage"
 	"github.com/go-chi/chi/v5"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	// 1. Veritabanı bağlantısını başlat
-	config.ConnectDB()
+	dbURL := os.Getenv("DATABASE_URL")
+	if dbURL == "" {
+		dbURL = "postgres://postgres:postgres@localhost:5432/go_contacts?sslmode=disable"
+	}
 
-	// 2. Router oluştur
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("DB bağlantı hatası: %v", err)
+	}
+	defer db.Close()
+
+	store := storage.NewPGStore(db)
+	h := &handler.ContactHandler{Store: store}
+
 	r := chi.NewRouter()
 
-	// 3. Contact handler'ı başlat
-	contactHandler := handler.NewContactHandler()
+	r.Post("/contacts", h.CreateContact)
+	r.Get("/contacts", h.GetAllContacts)
+	r.Get("/contacts/{id}", h.GetContactByID)
+	r.Put("/contacts/{id}", h.UpdateContact)
+	r.Delete("/contacts/{id}", h.DeleteContact)
 
-	// 4. Route'ları tanımla
-	r.Route("/contacts", func(r chi.Router) {
-		r.Post("/", contactHandler.CreateContact)
-		r.Get("/", contactHandler.GetAllContacts)
-		r.Get("/{id}", contactHandler.GetContactByID)
-		r.Put("/{id}", contactHandler.UpdateContact)
-		r.Delete("/{id}", contactHandler.DeleteContact)
-	})
-
-	// 5. Sunucuyu başlat
-	log.Println("Sunucu http://localhost:8080 adresinde çalışıyor")
+	// Sunucuyu başlat
+	log.Println("Sunucu http://localhost:8080 adresinde başlatıldı...")
 	http.ListenAndServe(":8080", r)
 }
